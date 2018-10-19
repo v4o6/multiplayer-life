eeee197530e949bcf38576460c2cae78b6a7832c
v4o6 <myron.yao@ze.com>
2018-10-19 11:59:11 -0700

    Spring Boot

---
+++ src/main/java/hackathon2018/multiplayerlife/MultiplayerLifeApplication.java
@@ -0,0 +1,12 @@
+package hackathon2018.multiplayerlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerLifeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MultiplayerLifeApplication.class, args);
+  }
+}
