4b956536d8a11db4349a1917ffd88fdf2283a701
Kenny Ho <kenny.ho@ze.com>
2018-10-22 16:09:12 -0700

    Update src/main/java/hackathon2018/multiplayerlife/service/GameService.java

--- src/main/java/hackathon2018/multiplayerlife/service/GameService.java
+++ src/main/java/hackathon2018/multiplayerlife/service/GameService.java
@@ -1,5 +1,9 @@
 package hackathon2018.multiplayerlife.service;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Comparator;
+import java.util.TreeMap;
+
 import org.springframework.stereotype.Service;
 
 import hackathon2018.multiplayerlife.entities.Game;
@@ -9,32 +13,73 @@ import hackathon2018.multiplayerlife.entities.Player;
 @Service
 public class GameService {
 
+  private static TreeMap<Long, Game> games =
+      new TreeMap<Long, Game>(Comparator.reverseOrder());
 
   public Player registerPlayer(final String name) {
-    // TODO
     // ... create player (no history/re-use; users create a new player for each game)
+    Player newPlayer = new Player();
+    newPlayer.setName(name);
     // ... associate player with a game
+    Game game;
+    if (games.isEmpty()) {
+      game = createNewGame();
+    }
+    else {
+      game = games.firstEntry().getValue();
+    }
+    while (!game.addPlayer(newPlayer)) {
+      game = createNewGame();
+      }
+    return newPlayer;
+  }
+
+  private SimpleEntry<Game, Player> getGamePlayerEntry(final long playerId) {
+    for (Game game : games.values()) {
+      for (Player player : game.getPlayers()) {
+        if (Long.compare(playerId, player.getId()) == 0) {
+          return new SimpleEntry<Game, Player>(game, player);
+        }
+      }
+    }
     return null;
   }
 
   public Player getPlayer(final long playerId) {
-    // TODO
-    return null;
-  }
-
-  public Game getGame(final long gameId) {
-    // TODO
+    SimpleEntry<Game, Player> GamePlayerEntry = getGamePlayerEntry(playerId);
+    if (GamePlayerEntry != null) {
+      return GamePlayerEntry.getValue();
+    }
     return null;
   }
 
   public Game getGameByPlayer(final long playerId) {
-    // TODO
+    SimpleEntry<Game, Player> GamePlayerEntry = getGamePlayerEntry(playerId);
+    if (GamePlayerEntry != null) {
+      return GamePlayerEntry.getKey();
+    }
+    return null;
+  }
+
+  public Game getGame(final long gameId) {
+    for (Game game : games.values()) {
+      if (Long.compare(game.getId(), gameId) == 0) {
+        return game;
+      }
+    }
     return null;
   }
 
   public void setPlayerState(final long playerId, final LifeState state) {
-    // TODO
+    Player player = getPlayer(playerId);
+    player.setState(state);
   }
 
+  private Game createNewGame() {
+    Game newGame = new Game();
+    Long newGameId = newGame.getId();
+    games.put(newGameId, newGame);
+    return newGame;
+  }
 
 }
