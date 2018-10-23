e7a4a6d6b1754377441a5abce4211431d905ee64
Fitsner <Fitsner@users.noreply.github.com>
2018-10-23 16:25:48 -0700

    Functions

--- src/main/java/hackathon2018/multiplayerlife/entities/Result.java
+++ src/main/java/hackathon2018/multiplayerlife/entities/Result.java
@@ -1,27 +1,36 @@
 package hackathon2018.multiplayerlife.entities;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Result {
 
-  private final LifeState state;
-  private final List<Player.Result> results;
+    private final LifeState state;
+    private final List<Player.Result> results;
 
-  public Result(final Game game, final LifeState state) {
-    this.state = state;
-    this.results = game.getPlayerResults(state);
-  }
+    public Result(final Game game, final LifeState state) {
+        this.state = state;
+        this.results = game.getPlayerResults(state);
+    }
 
-  public LifeState getState() {
-    return state;
-  }
+    public LifeState getState() {
+        return state;
+    }
 
-  public List<Player.Result> getResults() {
-    return results;
-  }
+    public List<Player.Result> getResults() {
+        return results;
+    }
 
-  public Player.Result getWinner() {
-    return null; // TODO dzmitry
-  }
+    public Player.Result getWinner() {
+        List<Integer> list = new ArrayList<>();
+        for (final Player.Result result : results) {
+            list.add(result.getCount());
+        }
+        int maxCount = Collections.max(list);
+        int indexOfMax = results.indexOf(maxCount);
+        return results.get(indexOfMax);
+    }
 
 }
